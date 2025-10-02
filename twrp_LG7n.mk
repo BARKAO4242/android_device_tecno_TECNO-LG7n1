82d50546eb74dd3f6b32a3122c352399a7ae1584
BARKAO4242 <erkocamuhammet83@gmail.com>
2025-10-02 11:00:16 +0300

    Update twrp_LG7n.mk

--- twrp_LG7n.mk
+++ twrp_LG7n.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from TECNO-LG7n device
+# Inherit from LG7n device
 $(call inherit-product, device/tecno/LG7n/device.mk)
 
 PRODUCT_DEVICE := LG7n
